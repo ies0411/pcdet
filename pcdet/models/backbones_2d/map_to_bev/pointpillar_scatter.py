a5433f7c754d7ffff38312b65281fb0f96e6e292
eunsoo.Im <eslim@superb-ai.com>
2024-04-16 19:57:00 +0900

    update

--- pcdet/models/backbones_2d/map_to_bev/pointpillar_scatter.py
+++ pcdet/models/backbones_2d/map_to_bev/pointpillar_scatter.py
@@ -12,7 +12,10 @@ class PointPillarScatter(nn.Module):
         assert self.nz == 1
 
     def forward(self, batch_dict, **kwargs):
-        pillar_features, coords = batch_dict['pillar_features'], batch_dict['voxel_coords']
+        pillar_features, coords = (
+            batch_dict["pillar_features"],
+            batch_dict["voxel_coords"],
+        )
         batch_spatial_features = []
         batch_size = coords[:, 0].max().int().item() + 1
         for batch_idx in range(batch_size):
@@ -20,11 +23,14 @@ class PointPillarScatter(nn.Module):
                 self.num_bev_features,
                 self.nz * self.nx * self.ny,
                 dtype=pillar_features.dtype,
-                device=pillar_features.device)
+                device=pillar_features.device,
+            )
 
             batch_mask = coords[:, 0] == batch_idx
             this_coords = coords[batch_mask, :]
-            indices = this_coords[:, 1] + this_coords[:, 2] * self.nx + this_coords[:, 3]
+            indices = (
+                this_coords[:, 1] + this_coords[:, 2] * self.nx + this_coords[:, 3]
+            )
             indices = indices.type(torch.long)
             pillars = pillar_features[batch_mask, :]
             pillars = pillars.t()
@@ -32,23 +38,30 @@ class PointPillarScatter(nn.Module):
             batch_spatial_features.append(spatial_feature)
 
         batch_spatial_features = torch.stack(batch_spatial_features, 0)
-        batch_spatial_features = batch_spatial_features.view(batch_size, self.num_bev_features * self.nz, self.ny, self.nx)
-        batch_dict['spatial_features'] = batch_spatial_features
+        batch_spatial_features = batch_spatial_features.view(
+            batch_size, self.num_bev_features * self.nz, self.ny, self.nx
+        )
+        batch_dict["spatial_features"] = batch_spatial_features
         return batch_dict
 
 
 class PointPillarScatter3d(nn.Module):
     def __init__(self, model_cfg, grid_size, **kwargs):
         super().__init__()
-        
+
         self.model_cfg = model_cfg
         self.nx, self.ny, self.nz = self.model_cfg.INPUT_SHAPE
         self.num_bev_features = self.model_cfg.NUM_BEV_FEATURES
-        self.num_bev_features_before_compression = self.model_cfg.NUM_BEV_FEATURES // self.nz
+        self.num_bev_features_before_compression = (
+            self.model_cfg.NUM_BEV_FEATURES // self.nz
+        )
 
     def forward(self, batch_dict, **kwargs):
-        pillar_features, coords = batch_dict['pillar_features'], batch_dict['voxel_coords']
-        
+        pillar_features, coords = (
+            batch_dict["pillar_features"],
+            batch_dict["voxel_coords"],
+        )
+
         batch_spatial_features = []
         batch_size = coords[:, 0].max().int().item() + 1
         for batch_idx in range(batch_size):
@@ -56,11 +69,16 @@ class PointPillarScatter3d(nn.Module):
                 self.num_bev_features_before_compression,
                 self.nz * self.nx * self.ny,
                 dtype=pillar_features.dtype,
-                device=pillar_features.device)
+                device=pillar_features.device,
+            )
 
             batch_mask = coords[:, 0] == batch_idx
             this_coords = coords[batch_mask, :]
-            indices = this_coords[:, 1] * self.ny * self.nx + this_coords[:, 2] * self.nx + this_coords[:, 3]
+            indices = (
+                this_coords[:, 1] * self.ny * self.nx
+                + this_coords[:, 2] * self.nx
+                + this_coords[:, 3]
+            )
             indices = indices.type(torch.long)
             pillars = pillar_features[batch_mask, :]
             pillars = pillars.t()
@@ -68,6 +86,11 @@ class PointPillarScatter3d(nn.Module):
             batch_spatial_features.append(spatial_feature)
 
         batch_spatial_features = torch.stack(batch_spatial_features, 0)
-        batch_spatial_features = batch_spatial_features.view(batch_size, self.num_bev_features_before_compression * self.nz, self.ny, self.nx)
-        batch_dict['spatial_features'] = batch_spatial_features
+        batch_spatial_features = batch_spatial_features.view(
+            batch_size,
+            self.num_bev_features_before_compression * self.nz,
+            self.ny,
+            self.nx,
+        )
+        batch_dict["spatial_features"] = batch_spatial_features
         return batch_dict
